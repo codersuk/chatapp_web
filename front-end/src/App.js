89e6e7fdfb0d55b64e730d4b7af156b185595265
Sergej-Vlasov <sergej.s.vlasov@gmil.com>
2019-03-24 15:21:31 +0000

    add socket.io-client and api.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { subscribeToTimer } from './api';
 
 class App extends Component {
   render() {
