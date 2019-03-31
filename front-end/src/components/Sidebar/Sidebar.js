b126cdee02897f4df02ca884b211d8b02f084750
Sergej-Vlasov <sergej.s.vlasov@gmail.com>
2019-03-31 14:13:11 +0100

    separate sidebar into 3 components

--- front-end/src/components/Sidebar/Sidebar.js
+++ front-end/src/components/Sidebar/Sidebar.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './sidebarStyles.scss';
+import Groups from './Groups';
+import Channels from './Channels';
+import DirectMessages from './DirectMessages';
 
 const sidebar = () => (
     <div className='sidebar'>
-        <div className='sidebar__items'>
-            <div className='sidebar__item'>
-                Workspace Title
-            </div>
-            <div className='sidebar__item'>
-                Available chanels:
-                <ul className='sidebar__item--chanel-list'>
-                    <li className='sidebar__item--chanel'>#chanel-name</li>
-                </ul>
-            </div>
+        <div className='sidebar-items'>
+            <Groups/>
+            <Channels/>
+            <DirectMessages/>
+            {/* <div className='sidebar-item sidebar-title'>Group Title</div>
+            <div className='sidebar-item sidebar-subtitle'>Available channels:</div>
+            <div className='sidebar-item--channel'>#channel-name</div>
+            <div className='sidebar-item sidebar-subtitle'>Direct messages</div>
+            <div className='sidebar-item--channel'>UserName</div> */}
         </div>
     </div>
 );
