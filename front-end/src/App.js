90b5cce92bba26b80579e74ecb6bb2170ef0f89b
Sergej-Vlasov <sergej.s.vlasov@gmail.com>
2019-03-31 11:32:00 +0100

    add basic sidebar layout

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.scss'
 import Input from './components/Input/Input'
 import Button from './/components/Button/Button'
+import Sidebar from './components/Sidebar/Sidebar';
 
 class App extends Component {
   state = {
@@ -19,6 +20,7 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
+        <Sidebar/>
         <Input change={this.getMessageHandler} />
         <Button click={this.sendMessageHandler} />
       </div>
--- front-end/src/components/Sidebar/Sidebar.js
+++ front-end/src/components/Sidebar/Sidebar.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import './sidebarStyles.scss';
 
-const sidebar = () => {};
+const sidebar = () => (
+    <div className='sidebar'>
+        <div className='sidebar__items'>
+            <div className='sidebar__item'>
+                Workspace Title
+            </div>
+            <div className='sidebar__item'>
+                Available chanels:
+                <ul className='sidebar__item--chanel-list'>
+                    <li className='sidebar__item--chanel'>#chanel-name</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+);
 
 export default sidebar;
