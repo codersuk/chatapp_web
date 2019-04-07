3312da79baccd8bce244d0812c06c1c2d8067e31
Sumia <sumia.017@gmail.com>
2019-04-07 15:17:05 +0100

    Added channel_perm_user link table

--- pythonbackend/mysite/channels/models.py
+++ pythonbackend/mysite/channels/models.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from mysite.Group.models import Group
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Channels(models.Model):
     name = models.CharField(max_length = 128)
     description = models.CharField(max_length = 256, default=None)
-    groupid = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    groupid = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+class channel_perm_user(models.Model):
+    userid = models.ForeignKey(User, on_delete=models.CASCADE)
+    channelid = models.ForeignKey(channelid)
+    perm = models.CharField(max_length=128)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
