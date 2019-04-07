254c91f48eab26ec8709d52cbf49ca0ae575332f
Sergej-Vlasov <sergej.s.vlasov@gmail.com>
2019-04-07 14:01:42 +0100

    add mock data and redered it in navbar

--- front-end/src/components/Sidebar/Sidebar.js
+++ front-end/src/components/Sidebar/Sidebar.js
@@ -4,6 +4,7 @@ import './sidebarStyles.scss';
 // import Channels from './Channels';
 // import DirectMessages from './DirectMessages';
 import { Navbar, Col, Row, Nav } from 'react-bootstrap';
+import { channelData, messagesData } from '../../mockData';
 
 const sidebar = () => (
     // <div className='sidebar'>
@@ -23,10 +24,26 @@ const sidebar = () => (
         <Col md='2'>
             <Navbar bg='dark' variant='dark' className='p-0'>
                 <Nav className='flex-column align-items-start'>
-                    <Nav.Link>Chat</Nav.Link>
                     <Nav.Link href='#'>Groups</Nav.Link>
+                    <Col md={{ offset: 1}} className='pb-3'>
+                    <Nav.Link href='#'>GroupList</Nav.Link>
+                    </Col>
                     <Nav.Link href='#'>Channels</Nav.Link>
+                    <Col md={{ offset: 1}} className='pb-3'>
+                    {channelData.map(channel => {
+                        if (channel.groupid === 9){
+                            return <Nav.Link href='#'>{channel.name}</Nav.Link>
+                        }
+                    })}
+                    </Col>
                     <Nav.Link href='#'>DirectMessages</Nav.Link>
+                    <Col md={{ offset: 1}} className='pb-3'>
+                    {messagesData.map(message => {
+                    if (message.channelid === 1292) {
+                        return <Nav.Link href='#'>{message.username}</Nav.Link>
+                    }
+                    })}
+                    </Col>
                 </Nav>
             </Navbar>
         </Col>
